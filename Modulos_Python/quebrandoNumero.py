f1f11c153025dd596dc98f132d9de6cf008cca82
miguel <guegosilvestre67@gmail.com>
2020-08-07 17:23:21 -0300

    adisionando algumas pata como POO e modolos

---
+++ Modulos_Python/quebrandoNumero.py
@@ -0,0 +1,8 @@
+from math import trunc
+
+numero = float(input('Digite um numero inteiro: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(numero, trunc(numero)))
+
+'''
+int (numero) => tambem funciona para estrair um inteiro
+'''
---
+++ Modulos_Python/utilizandoModulos.py
@@ -0,0 +1,8 @@
+from math import sqrt, floor
+#import math
+# math.sqrt
+
+numero = int(input('Digite um número: '))
+raiz = sqrt(numero)
+print('A raiz de {} é igual a {:.2f}'.format(numero, floor(raiz)))
+
